5d1cdec1e9d1037d206ce8d0db084d61d2a3a34a
DHRUV KUMAR <dkiphones620@gmail.com>
2017-10-01 16:07:43 +0530

    prevent scroling

--- js/app.js
+++ js/app.js
@@ -157,3 +157,10 @@ document.getElementById("touch").addEventListener('click', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
